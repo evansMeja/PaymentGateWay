e40adcf448a3355c4ff8ac17ab1ec3acefd76ef7
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-08-08 02:57:51 -0400

    first commit

--- payment_gateway/models.py
+++ payment_gateway/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class payments(models.Model):
-    amount = models.FloatField(default=0.0)
+    amount = models.CharField(max_length=100, blank=True,null=True)
     currency = models.CharField(max_length=100, blank=True,null=True)
-    number = models.IntegerField(blank=True,null=True)
-    status = models.CharField(max_length=100)
-    authorization_code = models.IntegerField()
-    time = models.DateTimeField()
+    number = models.CharField(max_length=100, blank=True,null=True)
+    status = models.CharField(max_length=100, blank=True,null=True)
--- payment_gateway/views.py
+++ payment_gateway/views.py
@@ -16,43 +16,56 @@ def js_approach(request):
     return render(request,template_name,context)
 
 def request_payment_api(request):
-    cardNumber = request.POST.get("cardNumber")
-    cardExpiryMonth = request.POST.get("cardExpiryMonth")
-    cardExpiryYear = request.POST.get("cardExpiryYear")
-    cardCVC = request.POST.get("cardCVC")
-    Amount = request.POST.get("Amount")
-    data = {'success':False}
-    url = PAYMENT_BACKENDS_SETTINGS['paypal']['url']
-    try:
-        task = {
-        "amount": Amount,
-        "currency": "USD",
-        "type": "creditcard",
-        "card": {
-            "number": cardNumber,
-            "expirationMonth": cardExpiryMonth,
-            "expirationYear": cardExpiryYear,
-            "cvv": cardCVC
-            }
-        }
-        response = requests.post(url, json=task)
-        if response.json()['status'] == "success":
-            data ['success']=True
-            amount = response.json()['amount']
-            currency = response.json()['currency']
-            number = response.json()['card']['number']
-            status = response.json()['status']
-            authorization_code = response.json()['authorization_code']
-            time = response.json()['time']
-            payments_obj = payments()
-            payments_obj.amount = amount
-            payments_obj.currency = currency
-            payments_obj.number=number
-            payments_obj.status=status
-            payments_obj.authorization_code = authorization_code
-            payments_obj.time= time
-            payments_obj.save()
-        data ['success']=True
-    except:
-            data ['success']=False
+    amount = request.GET.get("amount")
+    status = request.GET.get("status")
+    currency = request.GET.get("currency")
+    phone = request.GET.get("phone")
+    payments_obj = payments()
+    payments_obj.amount=amount
+    payments_obj.status=status
+    payments_obj.number=phone
+    payments_obj.currency = currency
+    payments_obj.save()
+    data={}
     return JsonResponse(data)
+# def request_payment_api(request):
+#     cardNumber = request.POST.get("cardNumber")
+#     cardExpiryMonth = request.POST.get("cardExpiryMonth")
+#     cardExpiryYear = request.POST.get("cardExpiryYear")
+#     cardCVC = request.POST.get("cardCVC")
+#     Amount = request.POST.get("Amount")
+#     data = {'success':False}
+#     url = PAYMENT_BACKENDS_SETTINGS['paypal']['url']
+#     try:
+#         task = {
+#         "amount": Amount,
+#         "currency": "USD",
+#         "type": "creditcard",
+#         "card": {
+#             "number": cardNumber,
+#             "expirationMonth": cardExpiryMonth,
+#             "expirationYear": cardExpiryYear,
+#             "cvv": cardCVC
+#             }
+#         }
+#         response = requests.post(url, json=task)
+#         if response.json()['status'] == "success":
+#             data ['success']=True
+#             amount = response.json()['amount']
+#             currency = response.json()['currency']
+#             number = response.json()['card']['number']
+#             status = response.json()['status']
+#             authorization_code = response.json()['authorization_code']
+#             time = response.json()['time']
+#             payments_obj = payments()
+#             payments_obj.amount = amount
+#             payments_obj.currency = currency
+#             payments_obj.number=number
+#             payments_obj.status=status
+#             payments_obj.authorization_code = authorization_code
+#             payments_obj.time= time
+#             payments_obj.save()
+#         data ['success']=True
+#     except:
+#             data ['success']=False
+#     return JsonResponse(data)
